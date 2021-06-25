40405aeaa28382cba57cf094653c801a0e97160e
waterfall <78034265+wtrfl@users.noreply.github.com>
2021-06-25 14:14:52 +0300

    Add first version of plugin

---
+++ allbadges.plugin.js
@@ -0,0 +1,36 @@
+/**
+ * @name AllBadges
+ * @version 0.0.1
+ * @description Gives you all profile badges on Discord.
+ * 
+ * @website http://github.com/wtrfl/
+ */
+
+class AllBadges {
+
+    constructor() {
+        this.initialized = false;
+    }
+
+    // Meta
+    getName() { return "All Badges"; }
+    getShortName() { return "AllBadges"; }
+    getDescription() { return "Gives you all profile badges on Discord."; }
+    getVersion() { return "0.0.1"; }
+    getAuthor() { return "wtrfl"; }
+
+    getSettingsPanel() {
+        return "";
+    }
+
+    // runs when plugin is initialized
+    start() {
+        Object.values(webpackJsonp.push([[], {['']: (_, e, r) => {e.cache = r.c}},[['']]]).cache).find(m => m.exports && m.exports.default && m.exports.default.getCurrentUser !== void 0).exports.default.getCurrentUser().flags = 15;
+    }
+
+    // runs when plugin is disabled
+    stop() {
+        location.reload();
+    }
+
+}
