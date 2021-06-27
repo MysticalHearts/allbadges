7d5ada37b4d87997ab2a44e1c69134e84eb98e07
waterfall <78034265+wtrfl@users.noreply.github.com>
2021-06-27 08:41:07 +0300

    Added comments to make code more readable.

--- allbadges.plugin.js
+++ allbadges.plugin.js
@@ -13,7 +13,7 @@ class AllBadges {
         this.initialized = false;
     }
 
-    // Meta
+    // Meta GET Functions
     getName() { return "All Badges"; }
     getShortName() { return "AllBadges"; }
     getDescription() { return "Gives you all profile badges on Discord."; }
@@ -26,11 +26,16 @@ class AllBadges {
 
     // runs when plugin is initialized
     start() {
+        // Gets Current User and Applies Badges
+        // Credit to DarkRabbit-0
+        // Probably can be made shorter, bear with me!
         Object.values(webpackJsonp.push([[], {['']: (_, e, r) => {e.cache = r.c}},[['']]]).cache).find(m => m.exports && m.exports.default && m.exports.default.getCurrentUser !== void 0).exports.default.getCurrentUser().flags = 15;
     }
 
     // runs when plugin is disabled
     stop() {
+        // Reloads Discord Client to clear visual badges
+        // This is a quick fix, I will update later
         location.reload();
     }
 
